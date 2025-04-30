3c736b03a2d8ede806e974e8df410e927af20272
zjp <jiping_zhou@foxmail.com>
2025-04-30 10:17:35 +0800

    remove $INSTALL dir and only install lockbud for testing push

--- os-checker.sh
+++ os-checker.sh
@@ -11,27 +11,27 @@ mkdir $INSTALL
 pushd $INSTALL
 echo $PWD
 
-# Install Rudra
-git clone https://github.com/os-checker/Rudra.git rudra
-pushd rudra
-cargo install --path . --locked
-cargo rudra --help
-popd
-
-# Install Mirai
-git clone https://github.com/os-checker/MIRAI.git mirai
-pushd mirai
-cargo install --path checker --no-default-features -F z3,z3-sys --locked
-cargo mirai --help
-popd
-
-# Install Rap
-git clone https://github.com/os-checker/RAPx.git rapx
-pushd rapx
-cd rapx
-cargo install --path .
-cargo rapx --help
-popd
+# # Install Rudra
+# git clone https://github.com/os-checker/Rudra.git rudra
+# pushd rudra
+# cargo install --path . --locked
+# cargo rudra --help
+# popd
+#
+# # Install Mirai
+# git clone https://github.com/os-checker/MIRAI.git mirai
+# pushd mirai
+# cargo install --path checker --no-default-features -F z3,z3-sys --locked
+# cargo mirai --help
+# popd
+#
+# # Install Rap
+# git clone https://github.com/os-checker/RAPx.git rapx
+# pushd rapx
+# cd rapx
+# cargo install --path .
+# cargo rapx --help
+# popd
 
 # Install Lockbud
 git clone https://github.com/os-checker/lockbud.git lockbud
@@ -44,6 +44,9 @@ popd
 # Back to $PWD
 popd
 echo $PWD
+# Clean all checker repos
+rm $INSTALL -rf
+
 # Set up default toolchain
 rustup toolchain install $DEFAULT_TOOLCHAIN
 rustup default $DEFAULT_TOOLCHAIN
