1366ff32d2dc80af60fb4edde2594d81ab0a3667
zjp <jiping_zhou@foxmail.com>
2025-04-30 08:35:14 +0800

    change path through pushd/popd

--- os-checker.sh
+++ os-checker.sh
@@ -3,37 +3,40 @@ set -xe
 export INSTALL=$PWD/install
 
 mkdir $INSTALL
+pushd $INSTALL
 
 # Install Rudra
 git clone https://github.com/os-checker/Rudra.git $INSTALL/rudra
-cd $INSTALL/rudra
-git switch master
+pushd $INSTALL/rudra
 cargo install --path . --locked
 cargo rudra --help
+popd
 
 # Install Mirai
 git clone https://github.com/os-checker/MIRAI.git $INSTALL/mirai
-cd $INSTALL/mirai
-git switch main
+pushd $INSTALL/mirai
 cargo install --path checker --no-default-features -F z3,z3-sys --locked
 cargo mirai --help
+popd
 
 # Install Rap
 
 git clone https://github.com/os-checker/RAP.git $INSTALL/rap
-cd $INSTALL/rap
-git switch main
+pushd $INSTALL/rap
 cd rap
 cargo install --path .
 cargo rap --help
+popd
 
 # Install Lockbud
 git clone https://github.com/os-checker/lockbud.git $INSTALL/lockbud
-cd $INSTALL/lockbud
+pushd $INSTALL/lockbud
 git switch all
 cargo install --path . --locked
 cargo lockbud --help
+popd
 
+popd
 # Set up default toolchain
 rustup default nightly-2025-04-30
 
